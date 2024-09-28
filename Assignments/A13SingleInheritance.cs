a44f51df552faaf72b547d24997eaf34d05e2df6
Shambela Sawant <shantanus871@gmail.com>
2024-09-28 15:26:52 +0530

    SingleInheritance

---
+++ Assignments/A13SingleInheritance.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    public class person
+    {
+       public int age;
+        public String name;
+
+    }
+    public class Student : person
+    {
+        public Student(int age,String name)
+        {
+            this.age = age;
+            this.name = name;
+            
+        }
+        public void Show()
+        {
+            Console.WriteLine(" The Age : " + age);
+            Console.WriteLine("The Name : " + name);
+        }
+
+    }
+    class A13SingleInheritance
+    {
+        public static void Main()
+        {
+            Student id = new Student(111, "Shambela");
+            id.Show();
+        }
+    }
+}
