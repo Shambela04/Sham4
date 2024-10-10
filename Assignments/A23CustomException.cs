0cdb763078b690c1408e2e1be655062f7711b81e
Shambela Sawant <shantanus871@gmail.com>
2024-10-10 12:32:37 +0530

    Exception Handling (custom Exception) (-ve no.)

---
+++ Assignments/A23CustomException.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    internal class NegativeNumberException : Exception
+    {
+        public NegativeNumberException(string? message) : base(message)
+        {
+
+        }
+    }
+
+
+    internal class A23CustomException
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Enter possitive number");
+                int i = Convert.ToInt32(Console.ReadLine());
+                if (i < 0)
+                {
+                    throw new NegativeNumberException("You Are Entered Negative Number ");
+                    
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            catch (NegativeNumberException ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                Console.WriteLine("Please Enter Positive Number : ");
+            }
+        }
+    }
+}
