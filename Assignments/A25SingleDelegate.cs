2f9dc08ed4e5c6760d2a149284ac681ce917fd21
Shambela Sawant <shantanus871@gmail.com>
2024-10-11 09:44:43 +0530

    Delegates Single Delegate Multicast Delegate

---
+++ Assignments/A25SingleDelegate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    public delegate void Add(int a, int b);
+    public delegate void Sub(int i, int j);
+
+    public class Delegate
+    {
+        public void Addition(int a, int b)
+        {
+            Console.WriteLine(a + b);
+
+        }
+
+        public void Substraction(int i, int j)
+        {
+            Console.WriteLine(i - j);
+            
+        }
+
+    }
+
+    internal class A25SingleDelegate
+    {
+        static void Main()
+        {
+            Delegate dg = new Delegate();
+            Add ad = new Add(dg.Addition);
+            Console.WriteLine("The Adiition is :");
+            ad(10,20);
+            
+
+            Delegate Dg = new Delegate();
+            Sub sb = new Sub(Dg.Substraction);
+            Console.WriteLine("The Substraction is :");
+            sb(20, 20);
+
+        }
+    }
+}
---
+++ Assignments/A26MulticastDelegate.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    public delegate void BasicOperations();
+
+    public class MultiDelegate
+    {
+        
+        public void Add1(int a,int b)
+        {
+            
+            Console.WriteLine($"Addition of Two Numbers : {a+b}");
+        }
+
+        public  void Sub(int c,int d)
+        {
+            Console.WriteLine($"Substraction of Two Numbers : {c-d} ");
+        }
+
+        public void Mult(int m,int n)
+        {
+            Console.WriteLine($"Multiplication of Two Numbers : {m*n}");
+        }
+
+
+    }
+
+    internal class A26MulticastDelegate
+    {
+        public static void Main()
+        {
+            MultiDelegate Md = new MultiDelegate();
+
+            
+
+            Md.Add1(10, 10);
+            Md.Sub(50, 10);
+            Md.Mult(6, 3);
+
+            Console.ReadKey();
+
+        }
+    }
+}
