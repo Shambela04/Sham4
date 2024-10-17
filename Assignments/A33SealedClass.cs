18a17a91eefb22b0220f6bd7efc9dda7bab58907
Shambela Sawant <shantanus871@gmail.com>
2024-10-17 14:11:30 +0530

    Sealed And Static Class Examples..

---
+++ Assignments/A33SealedClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+
+    class Baseclass
+    {
+        public void display()
+        {
+            Console.WriteLine("This is Base class");
+        }
+    }
+    sealed class DerivedClass : Baseclass
+    {
+        public void show()
+        {
+            Console.WriteLine("This is derived class : with Sealed class ");
+        }
+    }
+    class A33SealedClass
+    {
+        
+        static void Main()
+        {
+            DerivedClass sc = new DerivedClass();
+            sc.display();
+            sc.show();
+        }
+    }
+}
+
---
+++ Assignments/A34StaticClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+
+    static class Catworld
+    {
+        public static int NumberOfCats { get; private set; }
+        public static int TotalCatNaps { get; private set; }
+
+        static Catworld()
+        {
+            NumberOfCats = 20;
+        }
+        public static void AddCat()
+        {
+            NumberOfCats++;
+            Console.WriteLine($"Some cats are joined, Available cats are : {NumberOfCats}");
+        }
+        public static void AddCatNaps(int naps)
+        {
+            TotalCatNaps += naps;
+            Console.WriteLine($"Added {naps} cat naps, Total cat naps : {TotalCatNaps}");
+        }
+
+    }
+    internal class A34StaticClass
+    {
+
+        public static void Main()
+        {
+            Catworld.AddCat();
+            Catworld.AddCat();
+
+            Catworld.AddCatNaps(5);
+            Catworld.AddCatNaps(10);
+
+        }
+    }
+}
