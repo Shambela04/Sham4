e57269dc701070e4c0bbfe6578566cf5214a649b
Shambela Sawant <shantanus871@gmail.com>
2024-10-18 13:19:09 +0530

    Ref and Out Examples

---
+++ Assignments/A35RefExample.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    internal class A35RefExample
+    {
+        public static void IncrementValue(ref int i)
+        {
+             i++;
+
+        }
+        static void Main()
+        {
+            int I = 34;
+
+            Console.WriteLine($"Before increment : {I}");
+
+            IncrementValue(ref I);
+            
+            Console.WriteLine($"After increment :{I}");
+
+        }
+    }
+}
---
+++ Assignments/A36OutExample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    internal class A36OutExample
+    {
+        public static void Main()
+        {
+            
+            InitializeValue(out int a , out string b);
+
+            Console.WriteLine($"Year : {a}");
+            Console.WriteLine($"Organization : {b}");
+
+        }
+
+        static void InitializeValue(out int Value, out string Name)
+        {
+            Value = 2024;
+            Name = "CODEMIND TECHNOLOGY";
+        }
+    }
+}
