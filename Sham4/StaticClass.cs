c20f6c557281d28eb4e3b3fe2277285a90a67ef2
Shambela Sawant <shantanus871@gmail.com>
2025-03-05 19:28:56 +0530

    Static Class()

---
+++ Sham4/StaticClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sham4
+{
+    public static class Math
+    {
+        public static string add(string a)
+        {
+            return a ;
+
+        }
+        public static string Sub(string a, string b)
+        {
+            return a + b;
+        }
+
+    }
+    class StaticClass
+    {
+        static void Main()
+        {
+            string sum = Math.add("Sa want");
+            string diff = Math.Sub("sham","Bela");
+
+            Console.WriteLine($"Additoin :{sum}");
+            Console.WriteLine($"Join :{diff}");
+
+           
+        }
+    }
+}
