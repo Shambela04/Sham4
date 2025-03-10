fc3f6be21ae73cb644c7f6ea16f5b0328f15a4dd
Shambela Sawant <shantanus871@gmail.com>
2025-03-10 20:22:53 +0530

    Find MaxMin No.

---
+++ Sham4/MaxMinNo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sham4
+{
+    class MaxMinNo
+    {
+        public static void Main()
+        {
+            int[] no = { 1, 3, 4, 6, 9, 4, 98, };
+
+            int Max = no.Max();
+            int Min = no.Min();
+
+            Console.WriteLine(+Max);
+            Console.WriteLine(+Min);
+        }
+    }
+}
