ac5ec903cc572468bdda54485e257e708df805b2
Shambela Sawant <shantanus871@gmail.com>
2025-03-10 20:23:46 +0530

    Tuple

---
+++ Sham4/Tuple.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sham4
+{
+    public class Tuple
+    {
+        public static void Main()
+        {
+            var Tuple = (12, "Sham", "BEla", 04, 05, 2002, "Sawamt", 8.1,21,03,2000);
+
+            Console.WriteLine(Tuple.Item1);
+            Console.WriteLine(Tuple.Item2);
+            Console.WriteLine(Tuple.Item3);
+            Console.WriteLine(Tuple.Item4);
+            Console.WriteLine(Tuple.Item5);
+            Console.WriteLine(Tuple.Item6);
+            Console.WriteLine(Tuple.Item7);
+            Console.WriteLine(Tuple.Item8);
+            Console.WriteLine(Tuple.Item9);
+            Console.WriteLine(Tuple.Item10);
+            Console.WriteLine(Tuple.Item11);
+        }
+    }
+}
