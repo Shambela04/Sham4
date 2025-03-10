483f13c8bae1c6d7e3f9500626e1056aabce5048
Shambela Sawant <shantanus871@gmail.com>
2025-03-10 20:23:22 +0530

    Enum

---
+++ Sham4/EnumClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sham4
+{
+
+    using System;
+
+    class EnumClass
+    {
+        static void Main()
+        {
+            Weeks today = Weeks.Monday;
+            Console.WriteLine($"Today is: {today}");          // Output: Today is: Monday
+            Console.WriteLine($"Numeric Value: {(int)today}"); // Output: Numeric Value: 1
+
+            Weeks weekend = (Weeks)6; // Enum from integer
+            Console.WriteLine($"Day 6 is: {weekend}");        // Output: Day 6 is: Saturday
+        }
+    }
+
+    public enum Weeks
+    {
+        Sunday = 0,
+        Monday = 1,
+        Tuesday = 2,
+        Wednesday = 3,
+        Thursday = 4,
+        Friday = 5,
+        Saturday = 6
+    }
+
+
+
+}
